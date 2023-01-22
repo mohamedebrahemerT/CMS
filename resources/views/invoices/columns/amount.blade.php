cf612982d2b19147bf0ea6e0bb66367691269cd2
mohamedebrahemerT <mohamedchi2013@gmail.com>
2023-01-22 15:55:24 +0200

    asd
    sad

--- resources/views/invoices/columns/amount.blade.php
+++ resources/views/invoices/columns/amount.blade.php
@@ -1,6 +1,8 @@
 <div class="d-flex justify-content-end pe-22">
     @if(!empty($row->amount))
-        <p class="cur-margin"> {{ getCurrencySymbol().' '.number_format($row->amount) }}</p>
+        <p class="cur-margin">
+        {{ getCurrencySymbol() }} {{ number_format($row->amount - ($row->amount * $row->discount / 100),2)}}
+     </p>
     @else
         N/A
     @endif    
